7131b09a30fb7cea2b225d2424b6a53b0384939e
Jaewan Joo <jjw05015@gmail.com>
2024-01-29 15:51:35 +0900

    [BOJ] 구슬 탈출 / 골드 1 / 2시간
    알고리즘 분류 : 구현 / 그래프 이론 / 그래프 탐색 / 시뮬레이션 / 너비 우선 탐색
    Refactoring

--- JaewanJoo/BOJ13460.java
+++ JaewanJoo/BOJ13460.java
@@ -1,6 +1,9 @@
 import java.io.*;
 import java.util.*;
 
+// [BOJ] 구슬 탈출 / 골드 1 / 2시간
+// 알고리즘 분류 : 구현 / 그래프 이론 / 그래프 탐색 / 시뮬레이션 / 너비 우선 탐색
+// Refactoring
 public class Main {
 	static class Ball {
 		int row;
@@ -14,6 +17,13 @@ public class Main {
 		}
 	}
 	
+	static int holeRow, holeCol;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static char[][] board;
+	static boolean isNotMoved;
+	static boolean[] isOut;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		Queue<Ball[]> q = new LinkedList<>();
@@ -23,12 +33,10 @@ public class Main {
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		char[][] board = new char[n][m];
+		board = new char[n][m];
+		isOut = new boolean[2];
 		Ball[] balls = new Ball[2];
 		
-		int holeRow = -1, holeCol = -1;
-		int[] dr = {-1, 1, 0, 0};
-		int[] dc = {0, 0, -1, 1};
 		int ans = -1;
 		
 		for(int i = 0; i < n; i++) {
@@ -63,63 +71,35 @@ public class Main {
 			if(nowRed.cnt >= 10) break;
 			
 			for(int i = 0; i < 4; i++) {
-				boolean isRedOut = false;
-				boolean isBlueOut = false;
+				isOut[0] = false;
+				isOut[1] = false;
+				isNotMoved = true;
 				
-				int nextRedRow = nowRed.row;
-				int nextRedCol = nowRed.col;
-				int nextBlueRow = nowBlue.row;
-				int nextBlueCol = nowBlue.col;
+				int[] nextRedPos = {nowRed.row, nowRed.col};
+				int[] nextBluePos = {nowBlue.row, nowBlue.col};
 				
-				while(board[nextRedRow + dr[i]][nextRedCol + dc[i]] == '.') {
-					if(nextRedRow + dr[i] == nextBlueRow && nextRedCol + dc[i] == nowBlue.col) break;
-					
-					nextRedRow += dr[i];
-					nextRedCol += dc[i];
-					
-					if(nextRedRow == holeRow && nextRedCol == holeCol) isRedOut = true;
+				nextRedPos = rolling(i, 0, nextRedPos, nextBluePos);
+				nextBluePos = rolling(i, 1, nextBluePos, nextRedPos);
+				
+				if(!isOut[0] && board[nextRedPos[0] + dr[i]][nextRedPos[1] + dc[i]] == '.') {
+					nextRedPos = rolling(i, 0, nextRedPos, nextBluePos);
 				}
 				
-				while(board[nextBlueRow + dr[i]][nextBlueCol + dc[i]] == '.') {
-					if(!isRedOut && nextBlueRow + dr[i] == nextRedRow && nextBlueCol + dc[i] == nextRedCol) break;
-					
-					nextBlueRow += dr[i];
-					nextBlueCol += dc[i];
-					
-					if(nextBlueRow == holeRow && nextBlueCol == holeCol) isBlueOut = true;
+				if(!isOut[1] && board[nextBluePos[0] + dr[i]][nextBluePos[1] + dc[i]] == '.') {
+					nextBluePos = rolling(i, 1, nextBluePos, nextRedPos);
 				}
 				
-				if(!isRedOut && board[nextRedRow + dr[i]][nextRedCol + dc[i]] == '.') {
-					while(board[nextRedRow + dr[i]][nextRedCol + dc[i]] == '.') {
-						if(nextRedRow + dr[i] == nextBlueRow && nextRedCol + dc[i] == nextBlueCol) break;
-						
-						nextRedRow += dr[i];
-						nextRedCol += dc[i];
-						
-						if(nextRedRow == holeRow && nextRedCol == holeCol) isRedOut = true;
-					}
-				}
+				if(isNotMoved) continue;
 				
-				if(!isBlueOut && board[nextBlueRow + dr[i]][nextBlueCol + dc[i]] == '.') {
-					while(board[nextBlueRow + dr[i]][nextBlueCol + dc[i]] == '.') {
-						if(!isRedOut && nextBlueRow + dr[i] == nextRedRow && nextBlueCol + dc[i] == nextRedCol) break;
-						
-						nextBlueRow += dr[i];
-						nextBlueCol += dc[i];
-						
-						if(nextBlueRow == holeRow && nextBlueCol == holeCol) isBlueOut = true;
-					}
-				}
+				Ball nextRed = new Ball(nextRedPos[0], nextRedPos[1], nowRed.cnt + 1);
+				Ball nextBlue = new Ball(nextBluePos[0], nextBluePos[1], nowBlue.cnt + 1);
 				
-				Ball nextRed = new Ball(nextRedRow, nextRedCol, nowRed.cnt + 1);
-				Ball nextBlue = new Ball(nextBlueRow, nextBlueCol, nowBlue.cnt + 1);
-				
-				if(isRedOut && !isBlueOut) {
+				if(isOut[1]) {
+					continue;
+				} else if(isOut[0]) {
 					flag = true;
 					ans = nextRed.cnt;
 					break;
-				} else if(isBlueOut) {
-					continue;
 				}
 				
 				q.add(new Ball[] {nextRed, nextBlue});
@@ -130,5 +110,21 @@ public class Main {
 		
 		System.out.println(ans);
 		br.close();
+	}
+	
+	public static int[] rolling(int i, int rbIdx, int[] ballPos, int[] otherBallPos) {
+		int ballRow = ballPos[0], ballCol = ballPos[1];
+		int otherBallRow = otherBallPos[0], otherBallCol = otherBallPos[1];
+		while(board[ballRow + dr[i]][ballCol + dc[i]] == '.') {
+			if(!isOut[1- rbIdx] && ballRow + dr[i] == otherBallRow && ballCol + dc[i] == otherBallCol) break;
+			
+			isNotMoved = false;
+			
+			ballRow += dr[i];
+			ballCol += dc[i];
+			
+			if(ballRow == holeRow && ballCol == holeCol) isOut[rbIdx] = true;
+		}
+		return new int[] {ballRow, ballCol};
 	}
 }
