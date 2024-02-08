176eb467418b3a4f610b017110f0a04da5cf45b9
Jaewan Joo <jjw05015@gmail.com>
2024-02-08 10:47:39 +0900

    [BOJ] 주사위 굴리기 / 골드 4 / 2시간
    알고리즘 분류 : 구현 / 시뮬레이션

--- JaewanJoo/BOJ14499.java
+++ JaewanJoo/BOJ14499.java
@@ -1,6 +1,8 @@
 import java.io.*;
 import java.util.*;
 
+// [BOJ] 주사위 굴리기 / 골드 4 / 2시간
+// 알고리즘 분류 : 구현 / 시뮬레이션
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -8,17 +10,17 @@ public class Main {
 		StringTokenizer st;
 		
 		int[][] diceIdx = {
-			{1, 4, 6, 3},
-			{2, 4, 5, 3},
-			{6, 4, 1, 3},
-			{5, 4, 2, 3},
+			{0, 5, 0, 0},
+			{3, 1, 4, 6},
+			{0, 2, 0, 0},
+			{0, 6, 0, 0},
 		};
 		int[] dice = {0, 0, 0, 0, 0, 0, 0};
-		int[][] move = {{0, 0},{0, 1},{0, -1},{-1, 0}, {1, 0}};
+		int[][] move = {{0, 0}, {0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 		
-		int pitch = 0;
-		int roll = 0;
 		int command = 0;
+		int pitch = 1;
+		int roll = 1;
 		
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
@@ -37,20 +39,62 @@ public class Main {
 		
 		int top = 1;
 		int bottom = 6;
+		int tmp;
 		st = new StringTokenizer(br.readLine());
 		for(int i = 0; i < k; i++) {
+//			for(int j = 0; j < n; j++) {
+//				System.out.println(Arrays.toString(diceIdx[j]));
+//			}
+//			System.out.println("top = " + top + " ------------------");
 			command = Integer.parseInt(st.nextToken());
-			int nextX = x + move[command][0];
-			int nextY = y + move[command][1];
+			int dx = move[command][0];
+			int dy = move[command][1];
+			int nextX = x + dx;
+			int nextY = y + dy;
 			if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) continue;
 			
 			x = nextX;
 			y = nextY;
-			pitch = (pitch + move[command][0] + 4) % 4;
-			roll = (roll + move[command][1] + 4) % 4;
 			
+			pitch = pitch + dx;
+			roll = roll + dy;
 			top = diceIdx[pitch][roll];
 			bottom = 7 - top;
+			
+			switch(command) {
+			case 1: // 3146 -> 1463
+				tmp = diceIdx[1][0];
+				for(int j = 0; j < 3; j++) {
+					diceIdx[1][j] = diceIdx[1][j + 1];
+				}
+				diceIdx[1][3] = tmp;
+				diceIdx[3][1] = bottom;
+				break;
+			case 2: // 3146 -> 6314
+				tmp = diceIdx[1][3];
+				for(int j = 3; j >= 1; j--) {
+					diceIdx[1][j] = diceIdx[1][j - 1];
+				}
+				diceIdx[1][0] = tmp;
+				diceIdx[3][1] = bottom;
+				break;
+			case 3: // 5126 -> 6512
+				tmp = diceIdx[3][1];
+				for(int j = 3; j >= 1; j--) {
+					diceIdx[j][1] = diceIdx[j - 1][1];
+				}
+				diceIdx[0][1] = tmp;
+				diceIdx[1][3] = bottom;
+				break;
+			case 4: // 5126 -> 1265
+				tmp = diceIdx[0][1];
+				for(int j = 0; j < 3; j++) {
+					diceIdx[j][1] = diceIdx[j + 1][1];
+				}
+				diceIdx[3][1] = tmp;
+				diceIdx[1][3] = bottom;
+				break;
+			}
 			
 			if(map[x][y] == 0) {
 				map[x][y] = dice[bottom];
@@ -60,6 +104,8 @@ public class Main {
 			}
 			
 			sb.append(dice[top]).append("\n");
+			pitch = 1;
+			roll = 1;
 		}
 		
 		System.out.print(sb);
