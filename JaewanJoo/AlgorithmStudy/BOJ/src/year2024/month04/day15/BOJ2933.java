7f01d158b723a75c134a8461bfd781b1a90cd613
red-sprout <jjw05015@gmail.com>
2024-04-16 09:32:26 +0900

    [BOJ] 2933 미네랄 (골드 1)

--- JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month04/day15/BOJ2933.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month04/day15/BOJ2933.java
@@ -68,7 +68,6 @@ public class BOJ2933 {
 	public static void fallCluster(int[] result) {
 		if(result == null) return;
 		
-		int d = 101;
 		for(int i = 0; i < r; i++) {
 			Arrays.fill(visited[i], false);
 		}
@@ -77,49 +76,41 @@ public class BOJ2933 {
 		upper.clear();
 		for(int i = 0; i < c; i++) {
 			if(cave[r - 1][i] == '.' || visited[r - 1][i]) continue;
-			bfs(r - 1, i, lower);
+			bfs(r - 1, i);
 		}
 		
-		for(int i = 0; i < 4; i++) {
-			int nowRow = result[0] + dr[i];
-			int nowCol = result[1] + dc[i];
-			if(nowRow < 0 || nowRow >= r || nowCol < 0 || nowCol >= c) continue;
-			if(cave[nowRow][nowCol] == '.' || visited[nowRow][nowCol]) continue;
-			bfs(nowRow, nowCol, upper);
-		}
-		
-		for(int i = 0; i < c; i++) {
-			int lowerMin = r;
-			for(int[] pos : lower) {
-				if(pos[1] == i) {
-					lowerMin = Math.min(lowerMin, pos[0]);
-				}
+		for(int i = 0; i < r; i++) {
+			for(int j = 0; j < c; j++) {
+				if(cave[i][j] == '.' || visited[i][j]) continue;
+				upper.add(new int[] {i, j});
+				cave[i][j] = '.';
 			}
-			int upperMax = -1;
+		}
+		
+		for(int i = 0; i < r; i++) {
+			boolean isMove = true;
 			for(int[] pos : upper) {
-				if(pos[1] == i) {
-					upperMax = Math.max(upperMax, pos[0]);
+				if(pos[0] + 1 == r || cave[pos[0] + 1][pos[1]] == 'x') {
+					isMove = false;
+					break;
 				}
 			}
 			
-			if(upperMax == -1) continue;
-			d = Math.min(d, lowerMin - upperMax - 1);
+			if(!isMove) break;
+			for(int[] pos : upper) {
+				pos[0]++;
+			}
 		}
-
-		if(d == 0 || d == 101) return;
-		
-		Collections.sort(upper, (o1, o2)->(o2[0] - o1[0]));
 		
 		for(int[] pos : upper) {
-			cave[pos[0] + d][pos[1]] = 'x';
-			cave[pos[0]][pos[1]] = '.';
+			cave[pos[0]][pos[1]] = 'x';
 		}
 	}
 	
-	public static void bfs(int row, int col, List<int[]> cluster) {
+	public static void bfs(int row, int col) {
 		int[] start = new int[] {row, col};
 		q.add(start);
-		cluster.add(start);
+		lower.add(start);
 		visited[row][col] = true;
 		
 		while(!q.isEmpty()) {
@@ -132,7 +123,7 @@ public class BOJ2933 {
 				
 				int[] next = new int[] {nextRow, nextCol};
 				q.add(next);
-				cluster.add(next);
+				lower.add(next);
 				visited[nextRow][nextCol] = true;
 			}
 		}
