29dcfaf54c7bd343f851ad458275b2e606d4db60
red-sprout <jjw05015@gmail.com>
2024-04-29 07:37:23 +0900

    [BOJ] 15681 트리와 쿼리 (골드 5)

--- JaewanJoo/AlgorithmStudy/BOJ/src/Main.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/Main.java
@@ -2,26 +2,71 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static final long MAX = 1000_000_001;
+	private static int[] size;
+	private static Node[] nodes;
+	
+	private static class Node {
+		int idx;
+		List<Node> next = new ArrayList<>();
+		List<Node> child = new ArrayList<>();
+		
+		Node(int idx) {
+			this.idx = idx;
+		}
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
-		long x = Long.parseLong(st.nextToken());
-		long y = Long.parseLong(st.nextToken());
-		long z = y * 100 / x;
+		int n = Integer.parseInt(st.nextToken());
+		int r = Integer.parseInt(st.nextToken());
+		int q = Integer.parseInt(st.nextToken());
 		
-		long left = 0;
-		long right = MAX;
+		size = new int[n + 1];
+		nodes = new Node[n + 1];
 		
-		while(left < right) {
-			long mid = left + (right - left) / 2;
-			if((y + mid) * 100 / (x + mid) != z) right = mid;
-			else left = mid + 1;
+		for(int i = 1; i <= n; i++) {
+			nodes[i] = new Node(i);
 		}
 		
-		System.out.println(left == MAX ? -1 : left);
+		for(int i = 0; i < n - 1; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			nodes[a].next.add(nodes[b]);
+			nodes[b].next.add(nodes[a]);
+		}
+		
+		makeTree(nodes[r], nodes[r]);
+		countSubtreeNodes(nodes[r]);
+		
+		for(int i = 0; i < q; i++) {
+			int u = Integer.parseInt(br.readLine());
+			sb.append(size[u]).append("\n");
+		}
+		
+		System.out.print(sb.toString());
 		br.close();
+	}
+	
+	public static void makeTree(Node current, Node parent) {
+		for(Node node : current.next) {
+			if(node.idx != parent.idx) {
+				current.child.add(node);
+				makeTree(node, current);
+			}
+		}
+	}
+	
+	public static void countSubtreeNodes(Node current) {
+		size[current.idx] = 1;
+		for(Node node : current.child) {
+			countSubtreeNodes(node);
+			size[current.idx] += size[node.idx];
+		}
 	}
 }
