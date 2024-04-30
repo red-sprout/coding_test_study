4dbcd826c9e55c2b128859ab0a106b6ff759f91c
red-sprout <jjw05015@gmail.com>
2024-04-30 22:47:46 +0900

    [BOJ] 2798 블랙잭 [브론즈 2]

--- JaewanJoo/AlgorithmStudy/BOJ/src/Main.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/Main.java
@@ -2,71 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	private static int[] size;
-	private static Node[] nodes;
-	
-	private static class Node {
-		int idx;
-		List<Node> next = new ArrayList<>();
-		List<Node> child = new ArrayList<>();
-		
-		Node(int idx) {
-			this.idx = idx;
-		}
-	}
-	
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
-		int r = Integer.parseInt(st.nextToken());
-		int q = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int[] card = new int[n];
 		
-		size = new int[n + 1];
-		nodes = new Node[n + 1];
-		
-		for(int i = 1; i <= n; i++) {
-			nodes[i] = new Node(i);
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			card[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		for(int i = 0; i < n - 1; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			
-			nodes[a].next.add(nodes[b]);
-			nodes[b].next.add(nodes[a]);
-		}
-		
-		makeTree(nodes[r], nodes[r]);
-		countSubtreeNodes(nodes[r]);
-		
-		for(int i = 0; i < q; i++) {
-			int u = Integer.parseInt(br.readLine());
-			sb.append(size[u]).append("\n");
-		}
-		
-		System.out.print(sb.toString());
-		br.close();
-	}
-	
-	public static void makeTree(Node current, Node parent) {
-		for(Node node : current.next) {
-			if(node.idx != parent.idx) {
-				current.child.add(node);
-				makeTree(node, current);
+		int sum = 0;
+		int tmp = 0;
+		for(int i = 0; i < n - 2; i++) {
+			for(int j = i + 1; j < n - 1; j++) {
+				for(int k = j + 1; k < n; k++) {
+					tmp = card[i] + card[j] + card[k];
+					if(tmp > m) continue;
+					if(m - tmp < m - sum) sum = tmp;
+				}
 			}
 		}
-	}
-	
-	public static void countSubtreeNodes(Node current) {
-		size[current.idx] = 1;
-		for(Node node : current.child) {
-			countSubtreeNodes(node);
-			size[current.idx] += size[node.idx];
-		}
+		
+		System.out.println(sum);
+		
+		br.close();
 	}
 }
