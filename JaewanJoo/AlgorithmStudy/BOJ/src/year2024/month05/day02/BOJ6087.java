3d7b0ddf5f522cfcf3b969c7737b6ed827c3b649
red-sprout <jjw05015@gmail.com>
2024-05-02 01:36:12 +0900

    [BOJ] 6087 레이저 통신 (골드 3)

--- JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month05/day02/BOJ6087.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month05/day02/BOJ6087.java
@@ -7,6 +7,7 @@ public class BOJ6087 {
 	private static int w, h;
 	private static int[][] cnt;
 	private static char[][] map;
+	private static boolean[][] visited;
 	
 	private static int[] dr = {-1, 1, 0, 0};
 	private static int[] dc = {0, 0, -1, 1};
@@ -16,39 +17,67 @@ public class BOJ6087 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		w = Integer.parseInt(st.nextToken());
 		h = Integer.parseInt(st.nextToken());
-		map = new char[h][w];
 		cnt = new int[h][w];
+		map = new char[h][w];
+		visited = new boolean[h][w];
 		
 		for(int i = 0; i < h; i++) {
 			map[i] = br.readLine().toCharArray();
 			Arrays.fill(cnt[i], -1);
 		}
 		
-		List<int[]> list = new ArrayList<>();
+		int row = 0;
+		int col = 0;
 		for(int i = 0; i < h; i++) {
 			for(int j = 0; j < w; j++) {
 				if(map[i][j] == 'C') {
-					list.add(new int[] {i, j});
-					cnt[i][j] = 0;
+					row = i;
+					col = j;
+					break;
 				}
 			}
 		}
 		
-		bfs(list.get(0));
+		int endRow = 0;
+		int endCol = 0;
+		for(int i = 0; i < h; i++) {
+			for(int j = 0; j < w; j++) {
+				if(map[i][j] == 'C' && !(i == row && j == col)) {
+					endRow = i;
+					endCol = j;
+					break;
+				}
+			}
+		}
 		
+		System.out.println(bfs(row, col, endRow, endCol));
 		br.close();
 	}
 	
-	public static void bfs(int[] start) {
+	public static int bfs(int row, int col, int endRow, int endCol) {
 		Queue<int[]> q = new LinkedList<>();
-		q.add(start);
+		q.add(new int[] {row, col});
 		
 		while(!q.isEmpty()) {
 			int[] now = q.poll();
-			
 			for(int i = 0; i < 4; i++) {
-				
+				int nextRow = now[0] + dr[i];
+				int nextCol = now[1] + dc[i];
+				while(true) {
+					if(nextRow < 0 || nextRow >= h || nextCol < 0 || nextCol >= w) break;
+					if(map[nextRow][nextCol] == '*') break;
+					if(visited[nextRow][nextCol]) break;
+					if(cnt[nextRow][nextCol] != -1 && cnt[nextRow][nextCol] < cnt[now[0]][now[1]] + 1) break;
+					
+					if(cnt[nextRow][nextCol] != -1) visited[nextRow][nextCol] = true;
+					cnt[nextRow][nextCol] = cnt[now[0]][now[1]] + 1;
+					q.add(new int[] {nextRow, nextCol});
+					nextRow += dr[i];
+					nextCol += dc[i];
+				}
 			}
 		}
+		
+		return cnt[endRow][endCol];
 	}
 }
