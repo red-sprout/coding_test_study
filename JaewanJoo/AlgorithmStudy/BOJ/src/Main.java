b831ec7c0fef2468c09e77d615df18508982d0ce
red-sprout <jjw05015@gmail.com>
2024-05-04 00:27:09 +0900

    [BOJ] 2042 구간 합 (골드 1)

--- JaewanJoo/AlgorithmStudy/BOJ/src/Main.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/Main.java
@@ -2,34 +2,90 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	private static long[] arr;
+	private static long[] tree;
     public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int m = Integer.parseInt(st.nextToken());
-		int[] card = new int[n];
+		int k = Integer.parseInt(st.nextToken());
 		
-		st = new StringTokenizer(br.readLine());
-		for(int i = 0; i < n; i++) {
-			card[i] = Integer.parseInt(st.nextToken());
+		arr = new long[n + 1];
+		int h = (int)Math.ceil(Math.log(n) / Math.log(2));
+		int tree_size = (1 << (h + 1));
+		tree = new long[tree_size];
+		
+		for(int i = 1; i <= n; i++) {
+			arr[i] = Long.parseLong(br.readLine());
 		}
 		
-		int sum = 0;
-		int tmp = 0;
-		for(int i = 0; i < n - 2; i++) {
-			for(int j = i + 1; j < n - 1; j++) {
-				for(int k = j + 1; k < n; k++) {
-					tmp = card[i] + card[j] + card[k];
-					if(tmp > m) continue;
-					if(m - tmp < m - sum) sum = tmp;
-				}
+		init(1, 1, n);
+		for(int i = 0; i < m + k; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			
+			if(a == 1) {
+				// update
+				int b = Integer.parseInt(st.nextToken());
+				long c = Long.parseLong(st.nextToken());
+				update(1, 1, n, b, c);
+			} else if(a == 2) {
+				// query
+				int b = Integer.parseInt(st.nextToken());
+				int c = Integer.parseInt(st.nextToken());
+				sb.append(query(1, 1, n, b, c)).append("\n");
 			}
 		}
 		
-		System.out.println(sum);
-		
+		System.out.print(sb.toString());
 		br.close();
 	}
+    
+    public static void init(int node, int start, int end) {
+    	if(start == end) {
+    		tree[node] = arr[start];
+    		return;
+    	}
+    	
+    	init(node * 2, start, (start + end) / 2);
+    	init(node * 2 + 1, (start + end) / 2 + 1, end);
+    	
+    	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    }
+    
+    public static long query(int node, int start, int end, int left, int right) {
+    	if(left > end || right < start) {
+    		return 0;
+    	}
+    	
+    	if(left <= start && end <= right) {
+    		return tree[node];
+    	}
+    	
+    	long lsum = query(node * 2, start, (start + end) / 2, left, right);
+    	long rsum = query(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+    	
+    	return lsum + rsum;
+    }
+    
+    public static void update(int node, int start, int end, int index, long value) {
+    	if(index < start || index > end) {
+    		return;
+    	}
+    	
+    	if(start == end) {
+    		arr[index] = value;
+    		tree[node] = value;
+    		return;
+    	}
+    	
+    	update(node * 2, start, (start + end) / 2, index, value);
+    	update(node * 2 + 1, (start + end) / 2 + 1, end, index, value);
+    	
+    	tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    }
 }
