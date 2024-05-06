dbc33d386edafcf8c0630a50b236994d0cbcba4d
red-sprout <jjw05015@gmail.com>
2024-05-06 17:06:16 +0900

    [BOJ] 1000 A+B (브론즈 5)

---
+++ JaewanJoo/NodeJS/왕초보/BOJ1000.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const input = fs.readFileSync('example.txt')
+    .toString()
+    .trimEnd()
+    .split(' ')
+    .map(Number);
+
+const [a, b] = input;
+
+console.log(a + b);
