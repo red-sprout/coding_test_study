04f53623b427f19195cdce0b716a2b6873d30979
red-sprout <jjw05015@gmail.com>
2024-05-09 08:19:09 +0900

    tmp

--- JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month05/day09/BOJ18808.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month05/day09/BOJ18808.java
@@ -1,62 +1,47 @@
 package year2024.month05.day09;
-// 소용돌이 예쁘게 출력하기
+
 import java.io.*;
 import java.util.*;
 
 public class BOJ18808 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		String[] arr = br.readLine().split(" ");
+		StringTokenizer st;
 		
-		int r1 = Integer.parseInt(arr[0]);
-		int c1 = Integer.parseInt(arr[1]);
-		int r2 = Integer.parseInt(arr[2]);
-		int c2 = Integer.parseInt(arr[3]);
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
 		
-		int max = 0;
-		String[][] ans = new String[r2 - r1 + 1][c2 - c1 + 1];
-		for(int r = r1; r <= r2; r++) {
-			for(int c = c1; c <= c2; c++) {
-				int i = r - r1;
-				int j = c - c1;
-				
-				int x = Math.max(Math.abs(r), Math.abs(c));
-				int line = (x * 2 + 1);
-				int std = line * line;
-				int d = (x - r) + (x - c);
-
-				// row > col, 기준 - 거리
-				// row < col, 기준 - 4 * (한변 길이 - 1) + 거리;
-				if(r >= c) {
-					ans[i][j] = String.valueOf(std - d);
-				} else {
-					ans[i][j] = String.valueOf(std - 4 * (line - 1) + d);
-				}
-				
-				max = Math.max(ans[i][j].length(), max);
-			}
-		}
-
-		max++;
-		for(int i = 0; i <= r2 - r1; i++) {
-			for(int j = 0; j <= c2 - c1; j++) {
-				int length = ans[i][j].length();
-				for(int k = 0; k < max - length; k++) {
-					if(j == 0 && k == 0) continue;
-					ans[i][j] = " " + ans[i][j];
+		List<boolean[][]> list = new ArrayList<>();
+		for(int a = 0; a < k; a++) {
+			st = new StringTokenizer(br.readLine());
+			int r = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			boolean[][] sticker = new boolean[r][c];
+			for(int i = 0; i < r; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j = 0; j < c; j++) {
+					sticker[i][j] = st.nextToken().charAt(0) == '1';
 				}
 			}
 		}
 		
-		for(int i = 0; i <= r2 - r1; i++) {
-			for(int j = 0; j <= c2 - c1; j++) {
-				sb.append(ans[i][j]);
-			}
-			sb.append("\n");
+		for(int i = 0; i < 4; i++) {
+			
 		}
 		
-		System.out.print(sb.toString());
 		br.close();
+	}
+	
+	public static boolean[][] rot(boolean[][] now) {
+		boolean[][] next = new boolean[now[0].length][now.length];
+		for(int i = 0; i < now.length; i++) {
+			for(int j = 0; j < now[0].length; j++) {
+				next[j][now.length - 1 - i] = now[i][j];
+			}
+		}
+		return next;
 	}
 }
