f4fc419719022447798eac84d3d061888cf50e39
red-sprout <jjw05015@gmail.com>
2024-05-23 19:18:32 +0900

    [BOJ] 1933 스카이라인 (골드 1)

--- JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month05/day23/BOJ1933.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month05/day23/BOJ1933.java
@@ -4,30 +4,24 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ1933 {
-	private static class Building implements Comparable<Building> {
-		int left;
-		int height;
-		int right;
+	private static class Building implements Comparable<Building>{
+		int x, h;
 		
-		Building(int left, int height, int right) {
-			this.left = left;
-			this.height = height;
-			this.right = right;
+		Building(int x, int h) {
+			this.x = x;
+			this.h = h;
 		}
-		
+
 		@Override
 		public int compareTo(Building b) {
-			if(this.left == b.left) {
-				if(this.height == b.height) {
-					return this.right - b.right;
-				} else {
-					return b.height - this.height;
-				}
+			if(this.x == b.x) {
+				return b.h - this.h;
 			} else {
-				return this.left - b.left;
+				return this.x - b.x;
 			}
 		}
 	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		PriorityQueue<Building> pq = new PriorityQueue<>();
@@ -40,9 +34,37 @@ public class BOJ1933 {
 			int l = Integer.parseInt(st.nextToken());
 			int h = Integer.parseInt(st.nextToken());
 			int r = Integer.parseInt(st.nextToken());
-			pq.add(new Building(l, h, r));
+			pq.add(new Building(l, h));
+			pq.add(new Building(r, -h));
 		}
 		
+		TreeMap<Integer, Integer> map = new TreeMap<>(Collections.reverseOrder());
+		int maxX = 0;
+		int maxH = 0;
+		map.put(0, 1);
+		while(!pq.isEmpty()) {
+			Building b = pq.poll();
+			// 시작점
+			if(b.h > 0) {
+				map.put(b.h, map.getOrDefault(b.h, 0) + 1);
+			} else {
+				int val = map.get(-b.h);
+				if(val == 1) {
+					map.remove(-b.h);
+				} else {
+					map.replace(-b.h, val - 1);
+				}
+			}
+			
+			int height = map.firstKey();
+			if(maxX != b.x && maxH != height) {
+				sb.append(b.x).append(" ").append(height).append(" ");
+				maxX = b.x;
+				maxH = height;
+			}
+		}
+		
+		System.out.println(sb.toString());
 		br.close();
 	}
 }
