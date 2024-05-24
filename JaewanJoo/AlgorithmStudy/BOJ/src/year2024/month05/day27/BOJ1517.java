2af60f5826942a266a6a72b207ba1158c3975d39
red-sprout <jjw05015@gmail.com>
2024-05-24 21:46:56 +0900

    .

---
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month05/day27/BOJ1517.java
@@ -0,0 +1,21 @@
+package year2024.month05.day27;
+// 버블 소트
+import java.io.*;
+import java.util.*;
+
+public class BOJ1517 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+
+		// 5 1 6 3 4 (5)
+		/*
+		 * 
+		 */
+		
+		br.close();
+	}
+}
