ee16737d46a8f48ac610e0f4dbd383f21b152ee6
red-sprout <jjw05015@gmail.com>
2024-06-05 00:05:11 +0900

    [주재완] 14267 회사 문화 1 (골드 4)

--- JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month06/day03/BOJ14267.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month06/day03/BOJ14267.java
@@ -5,28 +5,51 @@ import java.util.*;
 
 public class BOJ14267 {
 	private static int n, m;
-	private static int[] boss, result;
+	private static List<List<Integer>> list;
+	private static int[] data;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		boss = new int[n + 1];
-		result = new int[n + 1];
+		list = new ArrayList<>();
+		for(int i = 0; i <= n; i++) {
+			list.add(new ArrayList<>());
+		}
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i = 1; i <= n; i++) {
-			int b = Integer.parseInt(st.nextToken());
-			boss[i] = (i == 1) ? 1 : b;
+			int boss = Integer.parseInt(st.nextToken());
+			if(boss != -1) {
+				list.get(boss).add(i);
+			}
 		}
 		
-		for(int i = 1; i <= n; i++) {
+		data = new int[n + 1];
+		for(int i = 0; i < m; i++) {
 			st = new StringTokenizer(br.readLine());
-			result[i] = Integer.parseInt(st.nextToken());
+			int man = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			data[man] += w;
 		}
 		
+		dfs(1);
+		for(int i = 1; i <= n; i++) {
+			sb.append(data[i]).append(" ");
+		}
+
+		System.out.println(sb.toString());
 		br.close();
+	}
+	
+	public static void dfs(int idx) {
+		for(int next : list.get(idx)) {
+			data[next] += data[idx];
+			dfs(next);
+		}
 	}
 }
