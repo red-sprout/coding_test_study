7122124266484327f82976f21b7708698ad9b762
red-sprout <jjw05015@gmail.com>
2024-06-13 08:19:14 +0900

    .

--- JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month06/day13/BOJ15684.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month06/day13/BOJ15684.java
@@ -5,62 +5,80 @@ import java.io.*;
 public class BOJ15684 {
 	private static int n, m, h;
 	private static BufferedReader br;
+	
 	public static void main(String[] args) throws IOException {
 		br = new BufferedReader(new InputStreamReader(System.in));
 		String[] info = br.readLine().split(" ");
 		n = Integer.parseInt(info[0]);
 		m = Integer.parseInt(info[1]);
 		h = Integer.parseInt(info[2]);
-		boolean[][] ladder = new boolean[n + 1][h + 1];
+		boolean[][] ladder = new boolean[h + 1][n + 1];
 		
+		int a, b;
 		for(int i = 0; i < m; i++) {
 			info = br.readLine().split(" ");
-			int a = Integer.parseInt(info[0]);
-			int b = Integer.parseInt(info[1]);
-			ladder[b][a] = true;
+			a = Integer.parseInt(info[0]);
+			b = Integer.parseInt(info[1]);
+			ladder[a][b] = true;
 		}
 		
-		for(int test = 0; test <= 3; test++) {			
-			dfs(ladder, 1, 0, test);
+		for(int test = 0; test <= 3; test++) {
+			dfs(1, 0, test, ladder);
 		}
+		
 		System.out.println(-1);
 		br.close();
 	}
 	
-	public static int getEnd(boolean[][] ladder, int row, int col) {
-		if (col == h + 1) return row;
-		if (row < n && ladder[row][col]) return getEnd(ladder, row + 1, col + 1);
-		if (row > 0 && ladder[row - 1][col]) return getEnd(ladder, row - 1, col + 1);
-		return getEnd(ladder, row, col + 1);
-	}	
-	
-	public static boolean isSameEnd(boolean[][] ladder) {
-		for(int i = 1; i <= n; i++) {
-			if(i != getEnd(ladder, i, 0)) return false;
-		}
-		return true;
-	}
-	
-	public static void dfs(boolean[][] ladder, int row, int cnt, int test) throws IOException {
+	public static void dfs(int row, int cnt, int test, boolean[][] ladder) throws IOException {
 		if(cnt == test) {
-			if(isSameEnd(ladder)) {			
-				System.out.println(cnt);
+			if(isSame(ladder)) {
+				System.out.println(test);
 				br.close();
 				System.exit(0);
 			}
 			return;
 		}
-
-		for(int i = 1; i <= n; i++) {
-			for(int j = row; j <= h; j++) {
+		
+		for(int i = row; i <= h; i++) {
+			for(int j = 1; j <= n; j++) {
 				if(ladder[i][j]) continue;
-				if(i == n) continue;
-				if(i < n && ladder[i + 1][j]) continue;
-				if(i > 0 && ladder[i - 1][j]) continue;
+				if(j == 1) {
+					if(ladder[i][j + 1]) continue;
+				} else if(j == n) {
+					continue;
+				} else {
+					if(ladder[i][j + 1] || ladder[i][j - 1]) continue;
+				}
+				
 				ladder[i][j] = true;
-				dfs(ladder, i, cnt + 1, test);
+				dfs(i, cnt + 1, test, ladder);
 				ladder[i][j] = false;
 			}
 		}
+	}
+	
+	public static boolean isSame(boolean[][] ladder) {
+		for(int i = 1; i <= n; i++) {
+			if(i != getEnd(0, i, ladder)) return false;
+		}
+		return true;
+	}
+	
+	public static int getEnd(int row, int col, boolean[][] ladder) {
+		if(row == h) {
+			return col;
+		}
+		
+		if(col == 1) {
+			if(ladder[row][col]) col++;
+		} else if(col == n) {
+			if(ladder[row][col - 1]) col--;
+		} else {
+			if(ladder[row][col]) col++;
+			else if(ladder[row][col - 1]) col--;
+		}
+		
+		return getEnd(row + 1, col, ladder);
 	}
 }
