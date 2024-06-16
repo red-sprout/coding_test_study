07b982f4deddd8ac00421ebf322fad82142f617c
red-sprout <jjw05015@gmail.com>
2024-06-17 00:13:59 +0900

    .

--- JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month06/day17/BOJ2243.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month06/day17/BOJ2243.java
@@ -1,5 +1,12 @@
 package year2024.month06.day17;
+// 사탕상자
+import java.io.*;
+import java.util.*;
 
 public class BOJ2243 {
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		br.close();
+	}
 }
