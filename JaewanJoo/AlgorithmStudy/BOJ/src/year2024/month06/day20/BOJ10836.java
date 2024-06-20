0d47b1cb93f5aaa1cf0590cabff6e32c151adec7
red-sprout <jjw05015@gmail.com>
2024-06-20 19:33:42 +0900

    .

---
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month06/day20/BOJ10836.java
@@ -0,0 +1,22 @@
+package year2024.month06.day20;
+// 여왕벌
+import java.io.*;
+
+public class BOJ10836 {
+	private static int m, n;
+	private static int[][] board, growth;
+	
+	public static void main(String[] args) {
+		input();
+	}
+	
+	public static void input() {
+		try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+}
