5b91ec17bafe5897859a7b49517980387cfd2398
red-sprout <jjw05015@gmail.com>
2024-06-21 23:24:04 +0900

    [BOJ] 10836 여왕벌 (골드 4)

--- JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month06/day20/BOJ10836.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month06/day20/BOJ10836.java
@@ -5,8 +5,7 @@ import java.util.*;
 
 public class BOJ10836 {
 	private static int m, n;
-	private static int[][] board, growth;
-	private static List<int[]> firstList;
+	private static int[][] board;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,59 +13,50 @@ public class BOJ10836 {
 		m = Integer.parseInt(info[0]);
 		n = Integer.parseInt(info[1]);
 		board = new int[m][m];
-		growth = new int[m][m];
-		firstList = new ArrayList<>();
 		
 		for(int i = 0; i < m; i++) {
 			Arrays.fill(board[i], 1);
 		}
 		
-		for(int i = m - 1; i > 0; i--) {
-			firstList.add(new int[] {i, 0});
-		}
-		
-		for(int i = 0; i < m; i++) {
-			firstList.add(new int[] {0, i});
-		}
-		
-		for(int i = 0; i < n; i++) {
+		for(int t = 0; t < n; t++) {
 			info = br.readLine().split(" ");
 			int zero = Integer.parseInt(info[0]);
 			int one = Integer.parseInt(info[1]);
 			int two = Integer.parseInt(info[2]);
-			init();
-			fillAll(new int[] {zero, one, two});
-		}
-		
-		print(board);
-		br.close();
-	}
-	
-	public static void init() {
-		for(int i = 0; i < m; i++) {
-			Arrays.fill(growth[i], 0);
-		}
-	}
-	
-	public static void fillAll(int[] count) {
-		int grow = 0;
-		for(int[] pos : firstList) {
-			while(count[grow] == 0) grow++;
-			growth[pos[0]][pos[1]] = grow;
-			count[grow]--;
+
+			for(int i = m - 1; i > 0; i--) { 
+				if(zero != 0) {
+					zero--;
+				} else if(one != 0) {
+					one--;
+					board[i][0] += 1;
+				} else if(two != 0) {
+					two--;
+					board[i][0] += 2;
+				}
+			}
+
+			for(int i = 0; i < m; i++) {
+				if(zero != 0) {
+					zero--;
+				} else if(one != 0) {
+					one--;
+					board[0][i] += 1;
+				} else if(two != 0) {
+					two--;
+					board[0][i] += 2;
+				}
+			}
 		}
 		
 		for(int i = 1; i < m; i++) {
 			for(int j = 1; j < m; j++) {
-				growth[i][j] = Math.max(growth[i - 1][j - 1], Math.max(growth[i - 1][j], growth[i][j - 1]));
+				board[i][j] = Math.max(board[i - 1][j - 1], Math.max(board[i - 1][j], board[i][j - 1]));
 			}
 		}
 		
-		for(int i = 0; i < m; i++) {
-			for(int j = 0; j < m; j++) {
-				board[i][j] += growth[i][j];
-			}
-		}
+		print(board);
+		br.close();
 	}
 	
 	public static void print(int[][] arr) {
