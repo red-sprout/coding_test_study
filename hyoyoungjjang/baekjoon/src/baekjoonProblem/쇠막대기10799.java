de82b4d36159b9a07a9c61894aa02ffc4ccbc381
DESKTOP-EFB2T8L\sunlang <khy20030930@gmail.com>
2024-06-25 21:58:04 +0900

    [BOJ] 10799/ 쇠막대기/ 1시간

---
+++ hyoyoungjjang/baekjoon/src/baekjoonProblem/쇠막대기10799.java
@@ -0,0 +1,35 @@
+package baekjoonProblem;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+//2024-06-25 20:50
+public class 쇠막대기10799 {
+	 public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 String[] sArr = br.readLine().split("");
+		 
+		 Stack<String> stack = new Stack<>();
+		 int result = 0;
+		 for(int i = 0; i < sArr.length; i++) {
+			String s = sArr[i];
+			if (s.equals("(")) {
+				stack.push(s);	
+				continue;
+			}
+			//이때 s는 ")"
+			
+			stack.pop();
+			if(sArr[i - 1].equals("(")) {
+				result += stack.size();
+			} else {
+				result += 1;
+			}
+		 }
+		 System.out.println(result);
+	 }
+}
+//2024-06-25 21:50
