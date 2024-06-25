21b48ff8daa69e4cf836b2e5353836baddbfd009
DESKTOP-EFB2T8L\sunlang <khy20030930@gmail.com>
2024-06-25 19:14:25 +0900

    [BOJ] 1935/ 후위표기식2/ 중간에 외식 다녀와서 까먹었어요

---
+++ hyoyoungjjang/baekjoon/src/baekjoonProblem/후위표기식1935.java
@@ -0,0 +1,49 @@
+package baekjoonProblem;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class 후위표기식1935 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Stack<Double> s = new Stack<>();
+		
+		int n = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		Double[] dArr = new Double[n];
+		for (int i = 0; i < dArr.length; i++) {
+			dArr[i] = Double.parseDouble(br.readLine());
+		}
+		
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch >= 'A' && ch <= 'z') {
+				s.push(dArr[ch - 65]);
+			} else {
+				double res = 0;
+				double a = s.pop();
+				double b = s.pop();
+				if(ch == '+') {
+					res = b + a;
+					s.push(res);
+				} else if (ch == '-') {
+					res = b - a;
+					s.push(res);
+				} else if (ch == '*') {
+					res = b * a;
+					s.push(res);
+				} else if (ch == '/') {
+					res = b / a;
+					s.push(res);
+				}
+			}
+		}
+		System.out.printf("%.2f", s.pop());
+		
+	} 
+}
