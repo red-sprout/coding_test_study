406255874aa73b1780a32394c3227e4b08dda03c
DrinkUpWater <ciwdj90@gmail.com>
2024-07-12 00:09:38 +0900

    [BOJ] 1931/ 회의실 배정 / 40분

--- DrinkUpWater/AlgorithmStudy/src/BOJ1931.java
+++ DrinkUpWater/AlgorithmStudy/src/BOJ1931.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ1931 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
