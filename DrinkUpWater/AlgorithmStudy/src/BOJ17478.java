9de2af8c32b164fed666fe04455ba8c4f3e139a3
DrinkUpWater <ciwdj90@gmail.com>
2024-07-14 15:28:54 +0900

    [BOJ] 17478/ 재귀함수가 뭔가요? / 30분

--- DrinkUpWater/AlgorithmStudy/src/BOJ17478.java
+++ DrinkUpWater/AlgorithmStudy/src/BOJ17478.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ17478 {
 	public static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
