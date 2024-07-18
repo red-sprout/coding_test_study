8de8b9359ad9d69405a823209bc44708ff589969
DrinkUpWater <ciwdj90@gmail.com>
2024-07-19 02:39:16 +0900

    [BOJ] 11729 / 하노이 탑 이동 순서 / 30분

--- DrinkUpWater/AlgorithmStudy/src/BOJ11729.java
+++ DrinkUpWater/AlgorithmStudy/src/BOJ11729.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ11729 {
 	private static StringBuilder sb;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
