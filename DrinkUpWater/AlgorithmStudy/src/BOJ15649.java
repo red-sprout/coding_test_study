1c8714291f1d15ca4e70bd83d53c978357c66e6b
DrinkUpWater <ciwdj90@gmail.com>
2024-07-19 19:27:31 +0900

    [BOJ] 15649/ n 과 m (1) / 1시간

--- DrinkUpWater/AlgorithmStudy/src/BOJ15649.java
+++ DrinkUpWater/AlgorithmStudy/src/BOJ15649.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ15649 {
 	private static int[] arr;
 	private static boolean[] isUsed;
 	
